19d43ba7361521b54dd80727706b1960ca2c078a
Bella01313 <ingabire.bella.muhizi@gmail.com>
2024-03-12 11:16:03 +0200

    first commit

---
+++ emoji.js
@@ -0,0 +1,9 @@
+// const changeFace = document.getElementById('face');
+const emojis = ["😊","😂","🤣","😁","😉","😎","😜","😘","😒","😍","❤","✌","😛","😌","😪","🤐","😐","😑",];
+const face = document.getElementById("face");
+const btn = document.getElementById("button");
+btn.addEventListener("click", () => {
+    let round = Math.floor(Math.random() * emojis.length);
+    let result = emojis[round];
+    face.textContent = result;
+});
